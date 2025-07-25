4e7fa02f5164e1f2b750dce98129d65d73554d85
firoz.nur <firoznur5@gmail.com>
2025-07-25 14:35:31 +0600

    Add : Catch Async (Avoid Repetition  Try Catch)

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -2,43 +2,63 @@ import { NextFunction, request, response } from "express";
 import httpStatus from "http-status-codes";
 import { userService } from "./user.service";
 import AppError from "../../errorHelpers/AppError";
+import { catchAsync } from "../../utils/catchAsync";
 
-const createUser = async (
-  req = request,
-  res = response,
-  next: NextFunction
-) => {
-  try {
-    // throw new Error("Fake Error");
-    // throw new AppError(httpStatus.BAD_REQUEST, "Fake Error");
+const createUser = catchAsync(async (req: Request, res: Response) => {
+  const user = await userService.createUser(req.body);
+  res
+    .status(httpStatus.CREATED)
+    .json({ message: "User created successfully", user });
+});
 
-    const user = await userService.createUser(req.body);
+// const createUser = async (
+//   req = request,
+//   res = response,
+//   next: NextFunction
+// ) => {
+//   try {
+//     // throw new Error("Fake Error");
+//     // throw new AppError(httpStatus.BAD_REQUEST, "Fake Error");
 
-    res
-      .status(httpStatus.CREATED)
-      .json({ message: "User created successfully", user });
-  } catch (error) {
-    console.log(error);
-    // res
-    //   .status(httpStatus.INTERNAL_SERVER_ERROR)
-    //   .json({ message: `Something went wrong ${error} from user controller`, error });
-    next(error);
+//     const user = await userService.createUser(req.body);
+
+//     res
+//       .status(httpStatus.CREATED)
+//       .json({ message: "User created successfully", user });
+//   } catch (error) {
+//     console.log(error);
+//     // res
+//     //   .status(httpStatus.INTERNAL_SERVER_ERROR)
+//     //   .json({ message: `Something went wrong ${error} from user controller`, error });
+//     next(error);
+//   }
+// };
+
+const getAllUsers = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const result = await userService.getAllUsers();
+
+    res.status(httpStatus.OK).json({
+      success: true,
+      message: "All Users Retrieved Successfully",
+      data: result,
+    });
   }
-};
+);
 
-const getAllUsers = async (
-  req = request,
-  res = response,
-  next: NextFunction
-) => {
-  try {
-    const users = await userService.getAllUsers();
-    res.status(httpStatus.OK).json({ users });
-  } catch (error) {
-    console.log(error);
-    next(error);
-  }
-};
+// const getAllUsers = async (
+//   req = request,
+//   res = response,
+//   next: NextFunction
+// ) => {
+//   try {
+//     const users = await userService.getAllUsers();
+//     res.status(httpStatus.OK).json({ users });
+//   } catch (error) {
+//     console.log(error);
+//     next(error);
+//   }
+// };
 
 export const userControllers = {
   createUser,
