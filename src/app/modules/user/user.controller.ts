c70fcbf097f92e5f4e645737d78fe2c6606b231e
firoz.nur <firoznur5@gmail.com>
2025-07-25 14:58:18 +0600

    Add : Send Response Utility

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -3,12 +3,20 @@ import httpStatus from "http-status-codes";
 import { userService } from "./user.service";
 import AppError from "../../errorHelpers/AppError";
 import { catchAsync } from "../../utils/catchAsync";
+import { sendResponse } from "../../utils/sendResponse";
 
 const createUser = catchAsync(async (req: Request, res: Response) => {
   const user = await userService.createUser(req.body);
-  res
-    .status(httpStatus.CREATED)
-    .json({ message: "User created successfully", user });
+  // res
+  //   .status(httpStatus.CREATED)
+  //   .json({ message: "User created successfully", user });
+
+  sendResponse(res, {
+    success: true,
+    statusCode: httpStatus.CREATED,
+    message: "User Created Successfully",
+    data: user,
+  });
 });
 
 // const createUser = async (
@@ -38,10 +46,18 @@ const getAllUsers = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
     const result = await userService.getAllUsers();
 
-    res.status(httpStatus.OK).json({
+    // res.status(httpStatus.OK).json({
+    //   success: true,
+    //   message: "All Users Retrieved Successfully",
+    //   data: result,
+    // });
+
+    sendResponse(res, {
       success: true,
+      statusCode: httpStatus.OK,
       message: "All Users Retrieved Successfully",
-      data: result,
+      data: result.data,
+      meta: result.meta,
     });
   }
 );
